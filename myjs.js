c992c52b8e08d79fe7aa9d94a1bbf0eea1d37a81
Jed Iquin <Jed Iquin>
2016-11-23 15:44:52 -0800

    added testimonials and support page

--- myjs.js
+++ myjs.js
@@ -1,8 +1,8 @@
 var main_content = document.getElementById("main_content");
 var overview = document.getElementById("overview");
 var specifications = document.getElementById("specs");
-//var overview = document.getElementById("overview");
-
+var support = document.getElementById("support");
+var testimonials = document.getElementById("testimonials");
 
 function response(){
     console.log("Ready!");
@@ -33,6 +33,49 @@ function response(){
             }
         })
     }
+    
+    support.onclick = function(){
+        $.ajax({
+            url: "support.html",
+            dataType: "html",
+            success: function(resp){
+                main_content.innerHTML = resp;
+                
+                var questions = document.getElementsByClassName("question");
+                
+                for(var x = 0 ; x < questions.length ; x++){
+                    console.log("hello");
+                    questions[x].onclick = toggle_answer;
+                }
+                
+                function toggle_answer(){
+                    var answer = this.nextElementSibling;
+                    console.log(answer.style.display);
+                    if(answer.style.display == "none" || !answer.style.display){
+                        answer.style.display = "block";
+                    }
+                    else{
+                        answer.style.display = "none";
+                    }
+                    console.log(answer.innerHTML)
+                }
+                
+            }
+        })
+    }
+    
+    testimonials.onclick = function(){
+        $.ajax({
+            url: "testimonials.html",
+            dataType: "html",
+            success: function(resp){
+                
+                main_content.innerHTML = resp;
+            }
+        })
+    }
+    
+    
     
 }
 
