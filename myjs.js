5bb489fc4b3049f3e6ea02df7c967c24b905d2d0
Jed Iquin <Jed Iquin>
2016-12-07 15:05:56 -0800

    modified specifactions page & added input validation for form

--- myjs.js
+++ myjs.js
@@ -53,6 +53,176 @@ function response(){
             success: function(resp){
                 main_content.innerHTML = resp;
                 
+                var info_image = document.getElementById("info_image");
+                var front_view_button = document.getElementById("front_view");
+                var back_view_button = document.getElementById("back_view");
+                var top_view_button = document.getElementById("top_view");
+                var left_view_button = document.getElementById("left_view");
+                var right_view_button = document.getElementById("right_view");
+                
+                
+                var spec_title = document.getElementById("spec_title");
+                var spec_info = document.getElementById("spec_info");
+                
+                function show_spec(spec){
+                    
+                    console.log(spec)
+                    spec_title.innerHTML = spec;
+
+                    switch(spec){
+                        case "Frame":
+                            spec_info.innerHTML = "The frame is made of something something something";
+                            break;
+                        case "Camera":
+                            spec_info.innerHTML = "A moving 8MP camera that has a vertical viewing angle of 60 degrees and horizontal viewing angle of 90 degrees";
+                            break;
+                        case "Lens":
+                            spec_info.innerHTML = "The Lens is made of a special type of glass that can filter certain wavelengths of light which can be controlled via sending electrical signals to it.";
+                            break;
+                        case "Eye Trackers":
+                            spec_info.innerHTML = "There are total of 8 eye trackers on the glasses, 4 on each lens. These eye trackers help with taking photos so you only get the images of where you are looking at, not where you are facing at.";
+                            break;
+                        case "Battery":
+                            spec_info.innerHTML = "eGlasses is using 3.7V 150mAh batter that is located on the right side of the glasses. At full charge, the eGlasses can be used up to 2 days of continuous use";
+                            break;
+                        case "Mode Change Button":
+                            spec_info.innerHTML = "Mode change button is a tactile button used to cycle through different modes of the glasses";
+                            break;
+                        case "Capture/Scan Button":
+                            spec_info.innerHTML = "Tactile button that serves as capture button in Photo Mode and Scan button in Scan & Translate Mode";
+                            break;
+                        case "USB Port":
+                            spec_info.innerHTML = "Conveniently placed USB Port for charging and connecting to computers for firmware upgrade";
+                            break;
+                        case "Arm":
+                            spec_info.innerHTML = "The Arm is made of something something something";
+                            break;
+                        case "Device ID":
+                            spec_info.innerHTML = "All eGlasses have a unique device ID so you know which devices you are connecting to your app";
+                            break;
+                        case "LED Status Indicator":
+                            spec_info.innerHTML = "Indicates the status of eGlasses when turned on. Steady green when above 30% battery power. Orange blinking when low battery. Red when charging. Blinking green when fully charged.";
+                            break;
+                        case "Power Button":
+                            spec_info.innerHTML = "Tactile button for turning the capabilities of your eGlasses on and off.";
+                            break;
+                    }
+                }
+                
+                
+                $.ajax({
+                    url: "front-view.html",
+                    dataType: "html",
+                    success: function(resp){
+                        
+                        info_image.innerHTML = resp;
+                        
+                        var frame_button = document.getElementById("frame_button");
+                        var camera_button = document.getElementById("camera_button");
+                        var lens_button = document.getElementById("lens_button");
+                        
+                        frame_button.onclick = function(){ return show_spec("Frame");}
+                        camera_button.onclick = function(){ return show_spec("Camera");}
+                        lens_button.onclick = function(){ return show_spec("Lens")}
+                    }
+                });
+                
+                front_view_button.onclick = function(){
+                    
+                    $.ajax({
+                        url: "front-view.html",
+                        dataType: "html",
+                        success: function(resp){
+                            info_image.innerHTML = resp;
+                            
+                            var frame_button = document.getElementById("frame_button");
+                            var camera_button = document.getElementById("camera_button");
+                            var lens_button = document.getElementById("lens_button");
+
+                            frame_button.onclick = function(){ return show_spec("Frame");}
+                            camera_button.onclick = function(){ return show_spec("Camera");}
+                            lens_button.onclick = function(){ return show_spec("Lens");}
+                        }
+                    })
+                }
+                
+                back_view_button.onclick = function(){
+                    
+                    $.ajax({
+                        url: "back-view.html",
+                        dataType: "html",
+                        success: function(resp){
+                            info_image.innerHTML = resp;
+                            
+                            var eye_trackers_button = document.getElementById("et_button");
+                            
+                            eye_trackers_button.onclick = function(){ return show_spec("Eye Trackers");}
+                        }
+                    })
+                }
+                
+                top_view_button.onclick = function(){
+                    
+                    $.ajax({
+                        url: "top-view.html",
+                        dataType: "html",
+                        success: function(resp){
+                            info_image.innerHTML = resp;
+                            
+                            
+                            
+                            var power_button = document.getElementById("power_button");
+                            var led_button = document.getElementById("led_button");
+                            
+                            power_button.onclick = function(){ return show_spec("Power Button");}
+                            led_button.onclick = function(){ return show_spec("LED Status Indicator");}
+                            
+                            
+                            
+                            var battery_button = document.getElementById("battery_button");
+                            var capture_button = document.getElementById("capture_button");
+                            var mode_button = document.getElementById("mode_button");
+                            
+                            battery_button.onclick = function(){ return show_spec("Battery");}
+                            
+                            mode_button.onclick = function(){ return show_spec("Mode Change Button");}
+                        }
+                    })
+                }
+                
+                left_view_button.onclick = function(){
+                    
+                    $.ajax({
+                        url: "left-view.html",
+                        dataType: "html",
+                        success: function(resp){
+                            info_image.innerHTML = resp;
+                            
+                            var usb_button = document.getElementById("usb_button");
+                            var arm_button = document.getElementById("arm_button");
+                            
+                            usb_button.onclick = function(){ return show_spec("USB Port");}
+                            arm_button.onclick = function(){ return show_spec("Arm");}
+                        }
+                    })
+                }
+                
+                right_view_button.onclick = function(){
+                    
+                    $.ajax({
+                        url: "right-view.html",
+                        dataType: "html",
+                        success: function(resp){
+                            info_image.innerHTML = resp;
+                            
+                            var id_button = document.getElementById("ID_button");
+                            var capture_button = document.getElementById("capture_button");
+                            
+                            id_button.onclick = function(){ return show_spec("Device ID");}
+                            capture_button.onclick = function(){ return show_spec("Capture/Scan Button");}
+                        }
+                    })
+                }
                 
             }
         })
@@ -95,12 +265,78 @@ function response(){
             success: function(resp){
                 
                 main_content.innerHTML = resp;
+                
+                var submit = document.getElementById("submit")
+                var testimonials_container = document.getElementById("testimonials_container");
+                var input_username = document.getElementById("username");
+                var input_comment = document.getElementById("comment");
+                
+                // getter when page loads
+                $.ajax({
+                    //tell ajax to use post
+                    //type of communication
+                    type: "post",
+                    data: {
+                        type: "get",
+                        studentid : "A00790108"
+                    },
+                    url: "http://www.bcitdigitalarts.ca/vote/server/citServ.php",
+                    dataType: "json",
+                    success: function(resp){
+                        
+                        testimonials_array = []; //CHANGE THIS
+                        
+                        for(var x = 0; testimonials_array.length; x++){
+                            username = ""; //CHANGE THIS
+                            comment = ""; //CHANGE THIS
+                            
+                            var new_div = document.createElement("div");
+                            var user_header = document.createElement("h4");
+                            var comment_p = document.createElement("p");
+                            
+                            user_header.className = "user";
+                            comment_p.className = "review";
+                            
+                            new_div.appendChild(user_header);
+                            new_div.appendChild(review);
+                            testimonials_container.appendChild(new_div);
+                            
+                        }
+                        
+                    }
+                });
+                
+                submit.onclick = function(){
+		
+                    username = input_username.value;
+                    comment = input_comment.value;
+                    
+                    if(username == "" || comment == ""){
+                        window.alert("Please enter a username and comment");
+                        return false;
+                    }
+                    
+                    $.ajax({
+                        //tell ajax to use post
+                        //type of communication
+                        type: "post",
+                        data: {
+                            type: "get",
+                            studentid : "A00790108"
+                        },
+                        url: "http://www.bcitdigitalarts.ca/vote/server/citServ.php",
+                        dataType: "json",
+                        success: function(resp){
+                            
+                            window.alert("Thank you! Your testimonials has been posted");
+                            input_comment.value = "";
+                            input_username.value = "";
+                        }
+                    });
+                }
             }
         })
     }
-    
-    
-    
 }
 
 $(document).ready(response);
---
+++ order.js
@@ -0,0 +1,287 @@
+var error_messages = ["Please enter your first name", //0
+					  "Please enter your last name", //1
+					  "Please enter a valid phone number", //2
+					  "Please enter your address", //3
+					  
+					  "Please enter a valid email address", //4
+					  "Please re-enter your email address", //5
+					  "Email address does not match", //6
+					  "Please enter a valid phone number", //7
+					  "Please enter a valid email address",//8
+					  "Incorrect credit card information"]; //9
+
+var shipping_info = ["fname","lname","phone", "address"];
+
+//EMAIL VARS
+var email = document.getElementById("email");
+var v_email = document.getElementById("v_email");
+
+var error_email = document.getElementById("error_email");
+var error_email_verify = document.getElementById("error_email_verify");
+
+
+//phone vars
+var phone_b = document.getElementById("phone_b");
+var phone_s = document.getElementById("phone_s");
+
+
+//credit card inputs
+var credit_card = document.getElementById("credit_card");
+var security_number = document.getElementById("security_number");
+var cc_error = document.getElementById("cc_error");
+
+//shipping checkbox
+var checkbox_same_shipping = document.getElementById("same_shipping");
+
+
+var email_valid = false;
+var emails_match = false;
+var phone_valid = false;
+
+var inputs_valid = true;
+
+function goto_summary(with_error){
+    if(with_error){
+        window.location.href = "orderpage_error.html";
+    }
+    else{
+        window.location.href= "order_summary.html";
+    }
+}
+
+function goto_fillup(){
+    window.location.href = "orderpage.html";
+}
+
+function place_order(){
+    processing_text.style.visibility = "visible";
+    setTimeout(goto_complete, 2000);
+}
+
+function goto_complete()
+{
+    window.location.href = "order_complete.html";
+}
+
+
+function copy_values(){
+    
+	for(var x = 0 ; x < shipping_info.length ; x++){
+		var id_b = shipping_info[x]+"_b";
+		var id_s = shipping_info[x]+"_s";
+		
+		var input_b = document.getElementById(id_b);
+		var input_s = document.getElementById(id_s);
+		
+		input_s.value = input_b.value;
+	}
+	
+	var optional_add_b = document.getElementById("optional_address_b");
+	var optional_add_s = document.getElementById("optional_address_s");
+	
+	optional_add_s.value = optional_add_b.value;
+	
+	var city_b = document.getElementById("city_b");
+	var state_b = document.getElementById("state_b");
+	var country_b = document.getElementById("country_b");
+	
+	var city_s = document.getElementById("city_s");
+	var state_s = document.getElementById("state_s");
+	var country_s = document.getElementById("country_s");
+	
+	city_s.selectedIndex = city_b.selectedIndex;
+	state_s.selectedIndex = state_b.selectedIndex;
+	country_s.selectedIndex = country_b.selectedIndex;
+	
+}
+
+function clear_shipping(){
+	for(var x = 0 ; x < shipping_info.length ; x++){
+		var id_s = shipping_info[x]+"_s";
+		
+		var input_s = document.getElementById(id_s);
+		
+		input_s.value = "";
+	}
+	
+	optional_add_s.value = "";
+}
+
+function process_info(index, input_element_id){
+	//console.log("checking input of element "+input_element_id);
+	
+	var input_element = document.getElementById(input_element_id); 
+	var error_output = document.getElementById("error_"+input_element_id);
+	
+	if(input_element.value == ""){
+		error_output.innerHTML = error_messages[index];
+		return false;
+	}
+	else{
+		error_output.innerHTML = "";
+	}
+	return true
+}
+
+function check_shipping_info(index){
+	
+	result = true;
+	
+	var id_b = shipping_info[index] + "_b"; 
+	var id_s = shipping_info[index] + "_s";
+	
+	result = process_info(index, id_b) && result;
+	result = process_info(index, id_s) && result;
+
+	return result;
+}
+
+function check_phone(number, elem){
+	var regex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+	
+	if(regex.test(number)){
+		elem.style.backgroundColor = "white";
+		return true;
+	}
+	else{
+		if(number == ""){
+			elem.style.backgroundColor = "white";
+			return false;
+		}
+		
+		elem.style.backgroundColor = "#f44e4e";
+		return false;
+	}
+	return true;
+}
+
+function is_email_valid(email_value){
+		
+	var regex = /^\w+@[a-zA-Z0-9_]+?\.[a-zA-Z]{2,3}$/;
+	
+	if(regex.test(email_value)){
+		email.style.backgroundColor = "white";	
+		return true;
+	}
+	else{
+		if(email_value == ""){
+			email.style.backgroundColor = "white";	
+			return false;
+		}
+		email.style.backgroundColor = "#f44e4e";
+		return false;
+	}
+	
+	return false;
+}
+
+function do_emails_match(){
+	
+	if(email.value != v_email.value){
+		v_email.style.backgroundColor = "#f44e4e";
+		return false;
+	}
+	else{
+		v_email.style.backgroundColor = "white";
+	}
+	return true;
+}
+
+function check_email(){
+
+	if(email.value == "" || !email_valid){
+		console.log("arey ou doing this?");
+		console.log("email value is " + email.value);
+		error_email.innerHTML = error_messages[4];
+		return false;
+	}
+	else{
+		error_email.innerHTML = "";		
+	}
+	
+	if(v_email.value == ""){
+		error_email_verify.innerHTML = error_messages[5];
+		return false;
+	}
+	else{
+		error_email_verify.innerHTML = "";
+	}
+	
+	if(email.value != v_email.value){
+		error_email_verify.innerHTML = error_messages[6];
+		return false;
+	}
+	
+	return true;
+	//check valid email address use regex
+}
+
+function check_credit_card(){
+    
+    console.log("hello");
+    var regex_cc = /^[0-9]{16}$/;
+    var regex_sn = /^[0-9]{3}$/;
+    
+    var valid_cc = regex_cc.test(credit_card.value);
+    var valid_sn = regex_sn.test(security_number.value)
+    
+    if(valid_cc && valid_sn){
+        cc_error.innerHTML = " ";
+        return true;
+        
+    }
+    
+    cc_error.innerHTML = error_messages[9];
+    return false;
+	
+}
+
+function check_inputs(){
+	
+	//check shipping info
+	for(var x = 0 ; x < shipping_info.length ; x++){
+		inputs_valid = check_shipping_info(x) && inputs_valid;
+	}
+		
+	inputs_valid = check_email() && inputs_valid;
+	
+	inputs_valid = check_credit_card() &&
+                   inputs_valid && 
+				   email_valid && 
+				   emails_match && 
+		           phone_valid;				   
+		
+	if(inputs_valid){
+        console.log("goto summary")
+		goto_summary(true);
+	}
+}
+
+checkbox_same_shipping.onclick = function(){
+	
+	if(checkbox_same_shipping.checked){
+		console.log("Copy them values!")
+		copy_values();
+	}
+	else{
+		console.log("Clear 'em values");
+		clear_shipping();
+	}
+}
+
+email.onkeyup = function(){
+	email_valid = is_email_valid(email.value);
+	console.log(email_valid);
+}
+
+v_email.onkeyup = function(){
+	emails_match = do_emails_match();
+}
+
+phone_s.onkeyup = function(){
+	phone_valid = check_phone(phone_s.value, this);
+}
+
+phone_b.onkeyup = function(){
+	phone_valid = check_phone(phone_b.value, this);
+}
